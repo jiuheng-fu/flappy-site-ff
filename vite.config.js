6f2ea6800353629af68ffbcb212a705d967176e1
fujiuheng <1060817990@qq.com>
2025-11-12 16:26:07 +0800

    add vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/', // 确保这是相对路径
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   server: {
     port: 9099,
     host: '0.0.0.0',
